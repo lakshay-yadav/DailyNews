61d3ab03c3332feee17d95f6a05bd30187bb3cf5
lakshay-yadav <92993560+lakshay-yadav@users.noreply.github.com>
2022-04-17 22:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("This is my index js file");
 
 // Initialize the news api parameters
 let source = 'the-times-of-india';
-let apiKey = 'd093053d72bc40248998159804e0e67d'
+let apiKey = 'b70e73607f76423b9182330b7d495a59'
 
 // Grab the news container
 let newsAccordion = document.getElementById('newsAccordion');
